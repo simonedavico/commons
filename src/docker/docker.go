04a34c67d0066e41c5ce22802c4e5d5087f4b483
Cerfoglg <gabrielecerfoglio225@gmail.com>
2015-12-02 16:22:20 +0100

    Added commons for minio operation and docker API

---
+++ src/docker/docker.go
@@ -0,0 +1,30 @@
+package docker
+
+import (
+	"github.com/fsouza/go-dockerclient"
+	"os"
+	"log"
+	"fmt"
+)
+
+func createDockerClient() docker.Client {
+	endpoint := "unix:///var/run/docker.sock"
+    client, err := docker.NewClient(endpoint)
+	if err != nil {
+		log.Fatal(err)
+		}
+	return *client
+	}
+
+func createDockerTLSClient() docker.Client {
+	path := os.Getenv("DOCKER_CERT_PATH")
+	endpoint := "tcp://"+os.Getenv("DOCKER_HOST")+":2376"
+    ca := fmt.Sprintf("%s/ca.pem", path)
+    cert := fmt.Sprintf("%s/cert.pem", path)
+    key := fmt.Sprintf("%s/key.pem", path)
+    client, err := docker.NewTLSClient(endpoint, cert, key, ca)
+	if err != nil {
+		log.Fatal(err)
+		}
+	return *client
+	}
