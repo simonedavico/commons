0b2ca51203d2f9e25be1e591fe0ae0de786b3e79
simonedavico <simonedavico@gmail.com>
2016-07-27 19:34:33 +0200

    Fixes malformed hash generator pom

--- minio/java/bin/get-dependencies.sh
+++ minio/java/bin/get-dependencies.sh
@@ -3,5 +3,5 @@
 RELEASE_VERSION=v-dev
 
 mkdir lib
-wget -q --no-check-certificate -O ./lib/keyname-hash-generator.jar \
-    http://github.com/benchflow/commons/releases/download/${RELEASE_VERSION}/keyname-hash-generator.jar
+wget -q --no-check-certificate -O ./lib/benchflow-keyname-hash-generator.jar \
+    http://github.com/benchflow/commons/releases/download/${RELEASE_VERSION}/benchflow-keyname-hash-generator.jar
